6bdb62b28f97694d6e405ea585855a3bc01bf8f2
GUSTAVO AUGUSTO SANTOS SILVA <57485910817@info.local>
2026-07-30 16:31:39 -0300

    layout cm api finalizada

--- reactlist/src/App.jsx
+++ reactlist/src/App.jsx
@@ -1,67 +1,94 @@
-import editIcon from "./assets/lapis.svg"
-import trashIcon from "./assets/lixeira.svg"
-import './App.css'
-import { useState } from "react"
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import editIcon from "./assets/lapis.svg";
+import trashIcon from "./assets/lixeira.svg";
+import "./App.css";
 
 function App() {
-  //states e variedades
-  //funções e effects
+  const [tasklist, setTasklist] = useState([]);
 
-const [tasklist, setTasklist] = useState([
-  {id: 1, descricao: "Revisar HTML Semântico"},
-  {id: 2, descricao: "Revisar ReactJS"},
-  {id: 3, descricao: "Revisar ReactJS"},
-  {id: 4, descricao: "Revisar React Native"},
-]);
+  // Buscar tarefas
+  const getTasks = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/taskpoin")
+      const dataAPI = response.data
 
+      setTasklist(dataAPI)
+    } catch (error) {
+      console.error("Erro ao buscar tarefas:", error);
+    }
+  };
 
+  // Criar tarefa
+  const createTasks = () => {};
 
+  // Atualizar tarefa
+  const putTask = () => {};
 
+  // Excluir tarefa
+  const deleteTask = () => {};
 
+  useEffect(() => {
+    getTasks();
+  }, []);
 
   return (
     <>
-      <header className='header-section'>
-        <h1 className='header-section__title'>React List</h1>
+      <header className="header-section">
+        <h1 className="header-section__title">React List</h1>
       </header>
 
-      <main className='body-section'>
-        <form className='cad-task'>
-          <input type="text" className="cad-task__entry" placeholder='Adicionar uma tarefa' />
-          <button className="cad-task__btn-confirm">adicionar</button>
+      <main className="body-section">
+        <form className="cad-task">
+          <input
+            type="text"
+            className="cad-task__entry"
+            placeholder="Adicione uma tarefa"
+          />
+
+          <button
+            type="submit"
+            className="cad-task__btn-confirm"
+          >
+            Adicionar
+          </button>
         </form>
 
-        <section className='cardlist'>
-          {
-            tasklist.map((task) => {
-              return 
+        <section className="cardlist">
+          {tasklist.map((task) => (
+            <article className="cardtask" key={task.id}>
+              <p className="cardtask__task-text">
+                {task.descricao}
+              </p>
 
-              
-            })
-          }
+              <div className="cardtask__icon-box">
+                <div className="cardlist__icon">
+                  <img
+                    src={editIcon}
+                    alt="Editar tarefa"
+                  />
+                </div>
 
-
-
-          <article className='cardtask'>
-            <p className="cardtask__task-text">Estudar propriedades em React</p>
-          <div className="cardtask__icon-box">  
-          <div className="cardlist__icon">
-            <img src={editIcon} alt="Imagem de uma caneta - ação editar tarefa" />
-            </div>  
-           <div className="cardlist__icon">
-            <img src={trashIcon} alt="Imagem de uma lixeira - ação excluir tarefa" />
-            </div> 
-          </div>
-          </article>
+                <div className="cardlist__icon">
+                  <img
+                    src={trashIcon}
+                    alt="Excluir tarefa"
+                  />
+                </div>
+              </div>
+            </article>
+          ))}
         </section>
       </main>
 
-      <footer className='footer-section'>
-        <p className='footer-section__right-text'>2026 React List - Todos os direitos</p>
+      <footer className="footer-section">
+        <p className="footer-section__right-text">
+          2026 React List - Todos os direitos reservados
+        </p>
       </footer>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
